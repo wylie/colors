950a435a7a04b5cb051a926c63ce5b354c509f3d
Wylie Fisher <WFisher@ebsco.com>
2015-08-13 14:57:32 -0400

    the beginnings of some JS

--- dev/script.js
+++ dev/script.js
@@ -2,4 +2,15 @@ window.onload = init;
 
 function init() {
 	console.log('SHAZAM');
+	var submit = document.getElementById('submit');
+	submit.onclick = getValue;
+}
+
+function getValue() {
+	var input = document.getElementById('input').value;
+	var inputSplit = input.split('#');
+	console.log(inputSplit[1]);
+
+	var liOne = document.getElementById('id-one');
+	liOne.setAttribute('style', 'background-color: #' + inputSplit[1]);
 }
